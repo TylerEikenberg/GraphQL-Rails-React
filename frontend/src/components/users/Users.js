271fb5b363c42d7a53b77a0a4198a2c40e5f9add
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-20 15:09:07 -0500

    created Users component

---
+++ frontend/src/components/users/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import './Users.css';
+
+// import userAvatar
+// import createUser
+
+function Users() {
+  return <div></div>;
+}
+
+export default Users;
