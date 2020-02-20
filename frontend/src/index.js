b4a9efd4b345ea83aa2f9f105748fd1486784617
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-20 14:56:35 -0500

    installed apollo and graphql

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './Pages';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
