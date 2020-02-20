b1058cd3eb60d100945c5091a7f9072114e56854
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-20 13:17:25 -0500

    adding types to user_type.rb

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,13 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :email, String, null: false
+    field :posts, [Types::PostType], null: true
+    field :post_count, Integer, null: true
+
+    def post_count
+      object.posts.size
+    end
   end
 end
