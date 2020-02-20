1c3402c1aabbb68f377cc55c40fe69ebcc81f270
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2020-02-20 14:59:06 -0500

    Added graphql stuff to index, wrapped app in apolloprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,21 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const link = createHttpLink({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
